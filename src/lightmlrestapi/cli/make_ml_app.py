87e2c6df1399d763ad91df85506cb47cb2fbb054
xavier dupré <xavier.dupre@gmail.com>
2017-12-05 09:32:02 +0100

    fix dummy app

--- src/lightmlrestapi/cli/make_ml_app.py
+++ src/lightmlrestapi/cli/make_ml_app.py
@@ -29,12 +29,12 @@ def start_mlrestapi(name='dummy', host='127.0.0.1', port=8081, nostart=False, ws
     can be tested with a dummy application (``app_name='dummy'``).
     """
     try:
-        from ..testing import dummy_application, dummy_application_image, dummy_application_fct
+        from ..testing import dummy_application, dummy_application_image, dummy_application_fct, dummy_application_neighbors
     except (ImportError, ValueError):
         folder = os.path.normpath(os.path.join(
             os.path.abspath(os.path.dirname(__file__)), "..", ".."))
         sys.path.append(folder)
-        from lightmlrestapi.testing import dummy_application, dummy_application_image, dummy_application_fct
+        from lightmlrestapi.testing import dummy_application, dummy_application_image, dummy_application_fct, dummy_application_neighbors
 
     if name == "dummy":
         # Dummy application.
@@ -70,9 +70,13 @@ def start_mlrestapi(name='dummy', host='127.0.0.1', port=8081, nostart=False, ws
                 "Unable to find function 'restapi_predict' in file '{0}'\n{1}".format(name, code))
         app = dummy_application_fct(mod.restapi_predict)
 
-    elif name == "dummy":
+    elif name == "dummyimg":
         # Dummy application with an image.
         app = dummy_application_image(options=options)
+
+    elif name == "dummyknn":
+        # Dummy application with neighbors.
+        app = dummy_application_neighbors()
 
     elif '.py' in name:
         raise NotImplementedError(
