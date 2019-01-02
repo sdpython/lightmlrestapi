9fc228134a12e89afd46ec5bc2152fbbefe6bd3b
xavier dupré <xavier.dupre@gmail.com>
2019-01-02 12:07:34 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import sphinx_modern_theme
+import sphinx_modern_theme_modified
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 
 
@@ -11,8 +11,8 @@ local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
 set_sphinx_variables(__file__, "lightmlrestapi", "Xavier Dupré", 2018,
-                     "sphinx_modern_theme", [
-                         sphinx_modern_theme.get_html_theme_path()],
+                     "sphinx_modern_theme_modified", [
+                         sphinx_modern_theme_modified.get_html_theme_path()],
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/lightmlrestapi/issues/%s', 'issue')),
                      title="lightmlrestapi", book=True)
